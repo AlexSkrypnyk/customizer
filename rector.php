a1b37cee3e9080f3200ebe4ad18b7b18940d54ba
Alex Skrypnyk <alex@drevops.com>
2026-08-13 11:35:59 +1000

    Replaced removed PHPUNIT_100 set with COMPOSER_BASED and dropped deprecated rule skips.

--- rector.php
+++ rector.php
@@ -13,13 +13,11 @@ declare(strict_types=1);
 use Rector\CodeQuality\Rector\ClassMethod\InlineArrayReturnAssignRector;
 use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
 use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
-use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
 use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\SetList;
-use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return static function (RectorConfig $config): void {
@@ -37,15 +35,13 @@ return static function (RectorConfig $config): void {
     SetList::DEAD_CODE,
     SetList::INSTANCEOF,
     SetList::TYPE_DECLARATION,
-    PHPUnitSetList::PHPUNIT_100,
+    PHPUnitSetList::COMPOSER_BASED,
   ]);
 
   $config->rule(DeclareStrictTypesRector::class);
 
   $config->skip([
     // Rules added by Rector's rule sets.
-    CountArrayToEmptyArrayComparisonRector::class,
-    DisallowedEmptyRuleFixerRector::class,
     InlineArrayReturnAssignRector::class,
     NewlineAfterStatementRector::class,
     NewlineBeforeNewAssignSetRector::class,
